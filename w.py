371f8ca594dfbbfb47a419c88800ecc73435418d
cl <244526298+christoph-lechner@users.noreply.github.com>
2026-05-24 14:47:08 +0200

    reorganisation, step 1

--- w.py
+++ w.py
@@ -5,6 +5,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 from nav import get_initial_course
 
+from scipy.cluster.hierarchy import dendrogram
+from sklearn.cluster import AgglomerativeClustering
+
+from collections import Counter
+
 def normalize_coords(d):
     """
     The JSON contains the geolocation data as Int, scaled by 1E6.
@@ -29,41 +34,6 @@ def spatial_filter_criterion(d):
     # check against rect containing the State of Hamburg
     is_in_bbox = (9.7<=longitude) and (longitude<=10.4) and (53.35<=latitude) and (latitude<=53.75)
     return is_in_bbox
-
-
-
-with open('data__20260521T0041.json','r') as fin:
-    data_all = json.load(fin)
-
-data = []
-for d in data_all:
-    d = normalize_coords(d)
-    #if spatial_filter_criterion(d):
-    #    data.append(d)
-    data.append(d)
-
-longitude = [_['longitude'] for _ in data]
-latitude  = [_['latitude']  for _ in data]
-
-fig,hax = plt.subplots(1)
-hax.plot(longitude, latitude, 'o')
-plt.show()
-
-X = np.vstack((np.array(longitude),np.array(latitude)))
-X = np.transpose(X)
-print(X)
-
-
-
-"""
-Cluster and plot dendrogram
-"""
-import numpy as np
-from matplotlib import pyplot as plt
-from scipy.cluster.hierarchy import dendrogram
-
-from sklearn.cluster import AgglomerativeClustering
-from sklearn.datasets import load_iris
 
 ### based on code from https://scikit-learn.org/stable/auto_examples/cluster/plot_agglomerative_dendrogram.html#sphx-glr-auto-examples-cluster-plot-agglomerative-dendrogram-py
 def plot_dendrogram(model, **kwargs):
@@ -95,6 +65,72 @@ def plot_dendrogram(model, **kwargs):
     # Plot the corresponding dendrogram
     dendrogram(linkage_matrix, **kwargs)
 
+def cluster_compute_center(id_cluster: int):
+    """
+    Compute "Geometric median" (minimizes L1 norm in 2-D).
+    (Note: do this only if longitude/latitude values are from a small area, like a city.)
+    """
+    # get all points in cluster (with hard-coded ID)
+    idx = [_ for _,x in enumerate(cluster_labels) if x==id_cluster]
+    if len(idx)==0:
+        raise ValueError(f'no cluster with id={id_cluster}')
+    points = X[idx,:]
+    print(points)
+
+    from scipy.optimize import minimize
+
+    def total_distance(center, points):
+        return np.sum(np.linalg.norm(points - center, axis=1))
+
+    initial_guess = points.mean(axis=0)
+
+    result = minimize(
+        total_distance,
+        initial_guess,
+        args=(points,)
+    )
+    return result.x
+
+def cluster_plot(*, hax, id_cluster: int, indicate_center=False, kwargs):
+    idx = [_ for _,x in enumerate(cluster_labels) if x==id_cluster]
+    if len(idx)==0:
+        print(f'warning: no cluster to plot for id={id_cluster}')
+    points = X[idx,:]
+    hax.plot(points[:,0], points[:,1], 'o', **kwargs)
+    if indicate_center:
+        center = cluster_compute_center(id_cluster)
+        hax.plot(center[0], center[1], '+', **kwargs)
+        return center
+
+
+
+
+with open('data__20260521T0041.json','r') as fin:
+    data_all = json.load(fin)
+
+data = []
+for d in data_all:
+    d = normalize_coords(d)
+    #if spatial_filter_criterion(d):
+    #    data.append(d)
+    data.append(d)
+
+longitude = [_['longitude'] for _ in data]
+latitude  = [_['latitude']  for _ in data]
+
+fig,hax = plt.subplots(1)
+hax.plot(longitude, latitude, 'o')
+plt.show()
+
+X = np.vstack((np.array(longitude),np.array(latitude)))
+X = np.transpose(X)
+print(X)
+
+
+
+"""
+Cluster and plot dendrogram
+"""
 #####
 # pre-compute pair-wise distances on surface of Earth, using great circle distance
 # https://scikit-learn.org/stable/modules/generated/sklearn.metrics.pairwise.haversine_distances.html
@@ -135,50 +171,12 @@ plt.show()
 
 
 # points that have no neighbor within the distance threshould count as single-element cluster with their own ID
-from collections import Counter
 counts_cluster_labels = Counter(cluster_labels)
 counts_cluster_labels = dict(
         sorted(counts_cluster_labels.items(), key=lambda _: _[1], reverse=True)
 )
 print(counts_cluster_labels)
 
-
-"""
-Compute "Geometric median" (minimizes L1 norm in 2-D).
-(Note: do this only if longitude/latitude values are from a small area, like a city.)
-"""
-def cluster_compute_center(id_cluster: int):
-    # get all points in cluster (with hard-coded ID)
-    idx = [_ for _,x in enumerate(cluster_labels) if x==id_cluster]
-    if len(idx)==0:
-        raise ValueError(f'no cluster with id={id_cluster}')
-    points = X[idx,:]
-    print(points)
-
-    from scipy.optimize import minimize
-
-    def total_distance(center, points):
-        return np.sum(np.linalg.norm(points - center, axis=1))
-
-    initial_guess = points.mean(axis=0)
-
-    result = minimize(
-        total_distance,
-        initial_guess,
-        args=(points,)
-    )
-    return result.x
-
-def cluster_plot(*, hax, id_cluster: int, indicate_center=False, kwargs):
-    idx = [_ for _,x in enumerate(cluster_labels) if x==id_cluster]
-    if len(idx)==0:
-        print(f'warning: no cluster to plot for id={id_cluster}')
-    points = X[idx,:]
-    hax.plot(points[:,0], points[:,1], 'o', **kwargs)
-    if indicate_center:
-        center = cluster_compute_center(id_cluster)
-        hax.plot(center[0], center[1], '+', **kwargs)
-        return center
 
 
 # fixed dummy position in Hamburg for dev purposes

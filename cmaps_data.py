add1282d939fb5dc2d004c660fd7f47bce24c9c9
cl <244526298+christoph-lechner@users.noreply.github.com>
2026-05-25 11:47:18 +0200

    added HTTP-based status checks to downloader

--- cmaps_data.py
+++ cmaps_data.py
@@ -4,11 +4,32 @@ import time
 import datetime
 import signal
 import threading
+import argparse
 from threading import Event
 from pathlib import Path
 from cmaps_data_core import get_cmaps_data
 
-datadir = Path('/home/cl/work/criticalmaps--richtungspfeil/cmdata/')
+
+cfg = {
+    # data goes here (specify absolute paths!)
+    'datadir': Path('/home/cl/work/criticalmaps--richtungspfeil/cmdata/'),
+
+    # For HTTP-based health checking (if enabled). Age of last event seen that is still "good". Note that this cannot be changed at runtime.
+    'healthcheck_maxage': 900,
+}
+
+parser = argparse.ArgumentParser()
+
+# HTTP-based monitoring of the current status
+# You get HTTP status 200 if everything is OK (events are being processed),
+# and HTTP status 500 if there is an issue.
+# Use for example "curl --head http://localhost:9999/check" to check.
+parser.add_argument('--status_port', type=int, help='simple HTTP server for remote status checking', default=None)
+args = parser.parse_args()
+
+if args.status_port:
+    # import here: break early when there are missing dependencies
+    from healthcheck import Healthcheck
 
 #####
 
@@ -47,7 +68,7 @@ def t_download_worker(*,stop_event):
     def get_fn():
         tnow = datetime.datetime.now()
         tnowstr = tnow.strftime('%Y%m%dT%H%M%S_%f') # '%f' is always 6 digits wide and padded w/ leading zeros
-        return datadir / ('data_'+tnowstr+'.json')
+        return cfg['datadir'] / ('data_'+tnowstr+'.json')
 
     # schedule first request to happen at the start of the next minute
     tnext = ceil_min( datetime.datetime.now() )
@@ -65,6 +86,10 @@ def t_download_worker(*,stop_event):
             data = get_cmaps_data()
             with open(fn_out,'w') as fout:
                 fout.write(data)
+
+            # heartbeat
+            if status['healthcheck']:
+                status['healthcheck'].heartbeat()
         except:
             # design idea: Networking issues with data request are ok, file I/O issues should be re-raised to stop the program
             raise
@@ -83,14 +108,26 @@ def main():
         time.sleep(1)
 
     t_dl.join()
+    if status['healthcheck']:
+        status['healthcheck'].stop_server()
 
     return
 
 if __name__=='__main__':
+    status={}
+    status['healthcheck'] = None
+    if args.status_port:
+        # After the maximum age has been reached, the status will transition
+        # from OK to error.
+        # Note that there will be HTTP 500 until after the first successful data download
+        status['healthcheck'] = Healthcheck(http_port=args.status_port, max_age=cfg['healthcheck_maxage'])
+        status['healthcheck'].start_server()
+
     # Install signal handlers
     # SIGTERM is handled for graceful termination
     # SIGINT  is handled for graceful termination (user pressed <Ctrl>-<C>)
     signal.signal(signal.SIGTERM, sighandler_term)
     signal.signal(signal.SIGINT,  sighandler_term)
+
 
     main()

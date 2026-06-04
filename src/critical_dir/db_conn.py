d1f904dfe64c0e98aa15177590cf16a93afb4580
cl <244526298+christoph-lechner@users.noreply.github.com>
2026-06-04 14:58:28 +0200

    added improved configuration system

--- src/critical_dir/db_conn.py
+++ src/critical_dir/db_conn.py
@@ -1,11 +1,11 @@
 import psycopg
+from .settings import settings
 
 def get_db_conn():
+    # Configure DSN via environment variable (see file ".env")
+    #
     # Password in ~/.pgpass, line format
     # hostname:port:database:username:password
     # !mode has to be 600!
-    conn = psycopg.connect(dbname = 'dev', 
-                           user = 'dev', 
-                           host= '192.168.2.253',
-                           port = 15432)
+    conn = psycopg.connect(str(settings.pg_dsn))
     return conn
---
+++ src/critical_dir/settings.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import PostgresDsn
+from pathlib import Path
+
+class Settings(BaseSettings):
+    pg_dsn: PostgresDsn
+
+    img_dir: Path
+    api_downloader_json_outdir: Path
+
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        # ?add env_file_encoding='utf-8'
+        env_prefix='CRITICAL_DIR_'
+    )
+
+# import this into your other files
+settings = Settings()

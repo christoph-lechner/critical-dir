49c198cfdc4f0203bfe802307ad9b1f44d6e7b20
cl <244526298+christoph-lechner@users.noreply.github.com>
2026-06-24 11:04:04 +0200

    minor improvements to caching code

--- src/critical_dir/api.py
+++ src/critical_dir/api.py
@@ -19,7 +19,7 @@ from critical_dir.db_conn import get_db_conn
 from critical_dir.settings import get_settings
 from critical_dir.exceptions import EInsufficientData
 from critical_dir.wip_subclusters import generate_subclusters
-from critical_dir.api_util import generate_cache_key
+from critical_dir.api_util import generate_cache_key, determine_cache_ttl
 
 class SubClustersResponseItem(BaseModel):
     N: int
@@ -221,8 +221,12 @@ def clusters_worker(*, ag, min_cluster_size:int=3, t0:datetime.datetime=None, us
             print(f'*** cache hit, key={key}')
             return cached_data_to_result(d_cache)
 
-        # the blocking_timeset is set to several times the expected worst-case time
-        lock = redis.lock(f'lock:{key}', timeout=900, blocking_timeout=200)
+        lock = redis.lock(
+                    f'lock:{key}',
+                    timeout=900,
+                    # the blocking_timeset is set to several times the expected worst-case time
+                    blocking_timeout=200,
+                )
         try:
             with lock:
                 print('got the lock')
@@ -240,9 +244,13 @@ def clusters_worker(*, ag, min_cluster_size:int=3, t0:datetime.datetime=None, us
                 except EInsufficientData:
                     print('got exception EInsufficientData -> DB has insufficient data to run analysis for given parameters')
                     d_cache = {'got_exception':True}
-                    raise
+                    raise # FIXME/TODO: correct to re-raise?
                 finally:
                     print(f'*** storing data in cache, key={key}')
+                    # add timestamp (in UNIX epoch) to data, for later implementation of advanced caching schemes
+                    # (for instance, one could serve data already somewhat 'stale' but still within the hard cache expiration time and trigger preparation of fresh data in the background)
+                    tsnow = datetime.datetime.now().timestamp()
+                    d_cache['timestamp'] = tsnow
                     redis.set(key, json.dumps(d_cache, cls=MyJSONEncoder), ex=ttl)
 
                 qq_d_cache = redis.get(key)
@@ -250,9 +258,8 @@ def clusters_worker(*, ag, min_cluster_size:int=3, t0:datetime.datetime=None, us
         except LockError:
             raise TimeoutError('unable to acquire lock')
 
-    # most recent data is cached with a short time-to-live (client refresh period is 30 seconds, using
-    # a little-bit-longer value so that the actual computation is not always triggered by the same client)
-    r = cached__get_analyzed_and_transformed_data(t0=epoch, ttl=35)
+    # most recent data is cached shorter time-to-live
+    r = cached__get_analyzed_and_transformed_data(t0=epoch, ttl=determine_cache_ttl(data_age=0))
 
     # We can benefit from the cache if time differences in this list are multiple of 15 seconds
     # (15 secs is "quantization" of time steps in rounding function)
@@ -273,8 +280,7 @@ def clusters_worker(*, ag, min_cluster_size:int=3, t0:datetime.datetime=None, us
         try:
             # TODO: find better variable names, these are confusing: r_historic,res_historic and on the other hand r_h for the final result
 
-            # using longer cache time-to-live here (not expecting updates of underlying data here)
-            r_historic = cached__get_analyzed_and_transformed_data(t0=epoch-age, ttl=900)
+            r_historic = cached__get_analyzed_and_transformed_data(t0=epoch-age, ttl=determine_cache_ttl(data_age=age))
         except EInsufficientData:
             # There is not enough data to cluster.
             # Try next set of historic data.

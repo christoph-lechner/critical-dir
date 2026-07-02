39bbb0c187a378cc0e9a4620ebdfc2b903134ebd
cl <244526298+christoph-lechner@users.noreply.github.com>
2026-07-03 01:44:02 +0200

    add tests for updated stats code

--- tests/docker-ing/test_ing.py
+++ tests/docker-ing/test_ing.py
@@ -3,7 +3,7 @@ from critical_dir.settings import get_settings
 from psycopg.rows import dict_row
 import os
 
-def test_ingestion(capsys):
+def test_analyze_ingestion(capsys):
     """
     First tests for ingestion script.
     After performing API request, inspect database.
@@ -27,11 +27,12 @@ def test_ingestion(capsys):
     datatable_nrows = res['c']
 
     ### get number of rows from stats table (and test that there were only INSERTs)
-    cur.execute(f"SELECT nrows_inserts,nrows_updates FROM {settings.statstable} WHERE total_status='1';")
+    cur.execute(f"SELECT nrows_inserts,nrows_updates,nrows_quarantine FROM {settings.statstable} WHERE total_status='1';")
     res = cur.fetchall()
     # after loading one file, we expect exact one line
     assert len(res)==1
     assert 0==res[0]['nrows_updates']
+    assert 0==res[0]['nrows_quarantine']
     statstable_nrows = res[0]['nrows_inserts']
 
     assert statstable_nrows == datatable_nrows
@@ -39,7 +40,7 @@ def test_ingestion(capsys):
     # test file describes 8 devices, so we expect 8 rows in DB
     assert 8==datatable_nrows
 
-def test_idempotence(capsys):
+def test_analyze_idempotence(capsys):
     """
     Evaluates the test for idempotence.
     As of June-2026, this test is performed by running the same import twice.
@@ -63,13 +64,12 @@ def test_idempotence(capsys):
     cur.execute(f"SELECT COUNT(*) AS c FROM criticalmaps_stats_test_idempotency WHERE total_status='1';")
     res = cur.fetchone()
     assert res is not None
-
     statstable_nrows = res['c']
     assert 2==statstable_nrows
 
     ### verify that operation that came first resulted in only INSERTs, second operation only UPDATEs (same dataset)
     # FIXME: hard-coded table name
-    cur.execute(f"SELECT ts,nrows_inserts,nrows_updates FROM criticalmaps_stats_test_idempotency WHERE total_status='1' ORDER BY ts ASC;")
+    cur.execute(f"SELECT ts,nrows_inserts,nrows_updates,nrows_quarantine FROM criticalmaps_stats_test_idempotency WHERE total_status='1' ORDER BY ts ASC;")
     res = cur.fetchall()
     assert len(res)==2
     #with capsys.disabled():
@@ -77,5 +77,38 @@ def test_idempotence(capsys):
 
     assert res[0]['nrows_inserts']==nrows_expected
     assert res[0]['nrows_updates']==0
+    assert res[0]['nrows_quarantine']==0
     assert res[1]['nrows_inserts']==0
     assert res[1]['nrows_updates']==nrows_expected
+    assert res[1]['nrows_quarantine']==0
+
+def test_analyze_badlatlng(capsys):
+    """
+    Evaluates the test with bad latitude/longitude.
+    """
+    conn = get_db_conn()
+    # (https://www.psycopg.org/psycopg3/docs/advanced/rows.html#row-factories)
+    cur = conn.cursor(row_factory=dict_row)
+
+    # rows in file (for this test, one row is violating constraints)
+    nrows_in_file = 8
+
+    # FIXME: hard-coded table names
+    for tblname in ['criticalmaps_data_test_badlat','criticalmaps_data_test_badlng']:
+        cur.execute(f'SELECT COUNT(*) AS c FROM {tblname};')
+        res = cur.fetchone()
+        assert res is not None
+        assert res['c'] == nrows_in_file-1
+
+
+    # FIXME: hard-coded table names
+    for tblname in ['criticalmaps_stats_test_badlat','criticalmaps_stats_test_badlng']:
+        cur.execute(f"SELECT ts,nrows_inserts,nrows_updates,nrows_quarantine FROM {tblname} WHERE total_status='1';")
+        res = cur.fetchall()
+        assert len(res)==1
+        #with capsys.disabled():
+        #    print(res)
+
+        assert res[0]['nrows_inserts']==nrows_in_file-1
+        assert res[0]['nrows_updates']==0
+        assert res[0]['nrows_quarantine']==1
